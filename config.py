fe4b376dfe928f88cb2bad9471b5198b87918053
Chikupandit6867 <155217462+Chikupandit6867@users.noreply.github.com>
2024-01-16 00:08:06 +0530

    Update config.py
    Signed-off-by: Chikupandit6867 <155217462+Chikupandit6867@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Chikupandit6867",
+    "https://github.com/Chikupandit6867/Nityu-music-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
